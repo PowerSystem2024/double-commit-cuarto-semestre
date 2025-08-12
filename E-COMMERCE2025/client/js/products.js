eb5a72dbc0863cead75f1acb64eaec1b1b040961
Franco Morales <134340520+Xeneixxe@users.noreply.github.com>
2025-08-12 00:07:33 -0300

    Update products.js functionality

--- E-COMMERCE2025/client/js/products.js
+++ E-COMMERCE2025/client/js/products.js
@@ -4,28 +4,28 @@ const productos=[
         nombre: "Naranjas",
         precio: 1.5,
         quanty: 10,
-        imagen: "/client/media/fruta1.jgp",
+        imagen: "/E-COMMERCE2025/client/media/fruta1.jpg",
     },
     {
         id: 2,
         nombre: "Bananas",
         precio: 1.2,
         quanty: 15,
-        imagen: "/client/media/fruta2.jpg"
+        imagen: "/E-COMMERCE2025/client/media/fruta2.jpg"
     },
     {
         id: 3,
         nombre: "Frutilla",
         precio: 1.0,
         quanty: 20,
-        imagen: "/client/media/fruta3.jpg"
+        imagen: "/E-COMMERCE2025/client/media/fruta3.jpg"
     },
     {
         id: 4,
         nombre: "Manzanas",
         precio: 1.0,
         quanty: 20,
-        imagen: "/client/media/fruta4.jpg"
+        imagen: "/E-COMMERCE2025/client/media/fruta4.jpg"
     },
 
 ];
