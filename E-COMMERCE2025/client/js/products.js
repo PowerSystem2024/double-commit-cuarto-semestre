7fba52816c1ec595aeff6f573a53433ed7cb29e9
Franco Morales <134340520+Xeneixxe@users.noreply.github.com>
2025-08-11 23:54:49 -0300

    Clase 1 JavaScript

---
+++ E-COMMERCE2025/client/js/products.js
@@ -0,0 +1,31 @@
+const productos=[
+    {
+        id: 1,
+        nombre: "Naranjas",
+        precio: 1.5,
+        quanty: 10,
+        imagen: "/client/media/fruta1.jgp",
+    },
+    {
+        id: 2,
+        nombre: "Bananas",
+        precio: 1.2,
+        quanty: 15,
+        imagen: "/client/media/fruta2.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Frutilla",
+        precio: 1.0,
+        quanty: 20,
+        imagen: "/client/media/fruta3.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Manzanas",
+        precio: 1.0,
+        quanty: 20,
+        imagen: "/client/media/fruta4.jpg"
+    },
+
+];
