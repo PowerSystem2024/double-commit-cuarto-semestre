e1ad4c574b7b63cf4200ee9c5be27c882675641d
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2025-08-25 16:15:16 -0300

    Hola mundo Node.js

---
+++ Node/01-HolaMundo/app.js
@@ -0,0 +1,7 @@
+const nombre = " Double Commit";
+console.log("Hola Mundo" + nombre);
+
+const nombre2 = " Cecilia, Gabi, Franco"
+console.log("Hola Mundo2" + nombre2);
+
+console.log("Hola Mundo3 desde Node, " + nombre + " somos " + nombre2);
