cd399981ba202953bd547f2e4d202a5ae3956fc9
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-27 14:02:13 -0300

    Clase 03 - Python - tipo str

--- ProyectosPython/clases/Clase01/tipo_bool.py
+++ ProyectosPython/clases/Clase01/tipo_bool.py
@@ -11,3 +11,12 @@ print(f'Valor: {valor}, Resultado: {resultado}')
 valor = 0.0
 resultado = bool(valor)
 print(f'Valor: {valor}, Resultado: {resultado}')
+
+# Tipo String -> False '', True -> demás valores
+valor = ''
+resultado = bool(valor)
+print(f'Valor: {valor}, Resultado: {resultado}')
+
+valor = 'hola'
+resultado = bool(valor)
+print(f'Valor: {valor}, Resultado: {resultado}')
