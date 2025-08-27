afd453b36f567dfc5f13ae8e9bce307c2823ffcd
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-27 14:20:52 -0300

    Clase 03 - Python - Concatenacipon Strings

---
+++ ProyectosPython/clases/Clase01/tipo_str.py
@@ -0,0 +1,7 @@
+# Profundizando en el tipo String
+# Concatenación automática en Python
+
+variable = 'Adiós'
+mensaje = 'Hola' + ' ' + 'gente! ' + variable # 'Hola ''Alumnos'
+mensaje += ', Terminamos..'
+print(mensaje)
