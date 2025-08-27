bc374db0af9fafb405ea5b84a4be5fecca8b1b94
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-27 13:59:56 -0300

    Clase 03 - Python - Contructor bool

---
+++ ProyectosPython/clases/Clase01/tipo_bool.py
@@ -0,0 +1,13 @@
+# Bool contine los valores de True y False
+# Los tipos numéricos, es false para el 0 (cero), true para los demás valores
+valor = 0
+resultado = bool(valor)
+print(f'Valor: {valor}, Resultado: {resultado}')
+
+valor = -1
+resultado = bool(valor)
+print(f'Valor: {valor}, Resultado: {resultado}')
+
+valor = 0.0
+resultado = bool(valor)
+print(f'Valor: {valor}, Resultado: {resultado}')
