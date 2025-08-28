cb166b77c213601dbee2ee7dbecab8693bd10bb6
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-28 00:06:31 -0300

    correción en ventana dialogo

--- Javascript/Avatar/utils/dialog.mjs
+++ Javascript/Avatar/utils/dialog.mjs
@@ -1,5 +1,5 @@
 // Método para crear un elemento dialog
-const crearDialogo = (contenido = "", titulo = "Resultado", img) => {
+const crearDialogo = (contenido = "", titulo = "Resultado") => {
   const dialogo = document.createElement("dialog");
   dialogo.innerHTML = `
         <div>
@@ -7,12 +7,7 @@ const crearDialogo = (contenido = "", titulo = "Resultado", img) => {
             <p>${titulo}</p>
             <span id="cerrar-dialogo" title="Cerrar diálogo" style="cursor: pointer;">✖</span>
             </header>
-            <aside style="display: flex; margin: 0 auto; justify-content: center; align-items: center;">
-            <img src="${
-              img || "../public/avatar-zuko.webp"
-            }" width="45" height="45" alt="Avatar" style="border-radius: 50%;" />
             <p style="padding: 24px">${contenido}</p>
-            </aside>
         </div>
     `;
   document.body.appendChild(dialogo);
