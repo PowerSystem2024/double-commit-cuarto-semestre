98085467c11b209187c71df3b1f9067ba205efb4
Franco Morales <134340520+Xeneixxe@users.noreply.github.com>
2025-09-02 19:28:49 -0300

    se actualizó método en ver carrito

--- E-COMMERCE2025/client/js/cart.js
+++ E-COMMERCE2025/client/js/cart.js
@@ -1,38 +1,42 @@
-    const modalOverlay = document.getElementById("modal-overlay");
-    const modalContainer = document.getElementById("modal-container");
-    const cartBtn = document.getElementById("cart-btn");
+const modalOverlay = document.getElementById("modal-overlay");
+const modalContainer = document.getElementById("modal-container");
+const cartBtn = document.getElementById("cart-btn");
 
-    const displayCart = () => {
+const displayCart = () => {
+  modalContainer.innerHTML = "";
+  // mostrar
+  modalContainer.style.display = "flex";
+  modalOverlay.style.display = "block";
 
-        modalContainer.innerHTML = "";
-        // mostrar 
-        modalContainer.style.display = "flex";
-        modalOverlay.style.display = "block";
+  //modal Header
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
 
-        //modal Header
-        const modalHeader = document.createElement('div');
-        modalHeader.className = 'modal-header';
+  const modalTitle = document.createElement("div");
+  modalTitle.innerText = "Carrito de Compras";
+  modalTitle.className = "modal-title";
 
-        const modalTitle = document.createElement('div');
-        modalTitle.innerText = 'Carrito de Compras';
-        modalTitle.className = 'modal-title';
+  const modalClose = document.createElement("div");
+  modalClose.innerText = "❌";
+  modalClose.className = "modal-close";
+  modalHeader.append(modalClose);
 
-        const modalClose = document.createElement('div');
-        modalClose.innerText = '❌';
-        modalClose.className = 'modal-close';
-        modalHeader.append(modalClose);
+  modalClose.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
 
+  modalOverlay.addEventListener("click", (e) => {
+    if (modalContainer && !modalContainer.contains(e.target)) {
+      modalContainer.style.display = "none";
+      modalOverlay.style.display = "none";
+    }
+  });
 
-        modalClose.addEventListener("click", () => {
-            modalContainer.style.display = "none";
-            modalOverlay.style.display = "none";
-        });
+  modalContainer.append(modalTitle, modalHeader);
+};
 
-        modalContainer.append(modalTitle, modalHeader);
-
-    };
-
-    cartBtn.addEventListener("click", () => {
-        console.log("Carrito clickeado");
-        displayCart();
-    });
+cartBtn.addEventListener("click", () => {
+  console.log("Carrito clickeado");
+  displayCart();
+});
