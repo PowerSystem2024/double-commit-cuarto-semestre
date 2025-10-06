4032513be280fa333d7e23ceb1666b003fda0245
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-05 21:18:34 -0300

    se agregó propiedades de la aplicación: Java (tienda_libros)

---
+++ ProyectosJava/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/domain/Usuario.java
@@ -0,0 +1,22 @@
+package utn.tienda_libros.domain;
+
+public class Usuario {
+    public int count = 0;
+    private int id;
+    private String nombre;
+    private String correo;
+    public Usuario() {}
+
+    public Usuario(Integer id, String nombre, String correo) {
+        this.id = ++Usuario.this.count;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                
+                """;
+    }
+}
