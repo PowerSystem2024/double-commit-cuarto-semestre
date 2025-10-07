1685308f736f31a343e97a805fb8c0330f1c5a50
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-10-07 15:53:06 -0300

    Proyecto Tienda de Libros con SpringBoot y Swing -> Parte 03 [8.1] ✅ Lesto

---
+++ ProyectosJava/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -0,0 +1,33 @@
+package utn.tienda_libros.vista;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import utn.tienda_libros.servicio.LibroServicio;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Component
+public class LibroForm extends JFrame {
+    LibroServicio libroServicio;
+    private JPanel panel;
+
+    @Autowired
+    public LibroForm(LibroServicio libroServicio) {
+        this.libroServicio = libroServicio;
+        iniciarForma();
+    }
+
+    private void iniciarForma() {
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(800, 600);
+        // Para obtener las dimensiones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x, y);
+    }
+}
